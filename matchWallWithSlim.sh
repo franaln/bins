61bb54a0dcf3783b11838255f8dd7c25a9397d52
franaln <franaln@gmai.com>
2013-07-19 00:57:29 -0300

    update

--- matchWallWithSlim.sh
+++ matchWallWithSlim.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
 
-wall=$(cat $HOME/.config/nitrogen/bg-saved.cfg | grep file | sed 's:file=::g')
+wall=$(cat /home/fran/.config/nitrogen/bg-saved.cfg | grep file | sed 's:file=::g')
+
+unlink /usr/share/slim/themes/default/background.jpg
+
 ln -s $wall /usr/share/slim/themes/default/background.jpg
