d5a523b09b3628909f48ae87228dcaddb88390be
franaln <franaln@gmai.com>
2014-01-20 12:31:51 +0100

    update

--- reduce_images.sh
+++ reduce_images.sh
@@ -1,9 +1,34 @@
-#! /usr/bin/env bash
+#! /usr/bin/bash
 
-mkdir -p half
+if [ $# -lt 2 ] ; then
+    echo "Usage: reduce_images.sh directory percentage"
+    exit 0
+fi
 
-for i in $( ls *.JPG); do 
-    echo "reducing size: $i"
-    convert -resize 50% $i half/$i
+images_dir=$1
+perc=$2
+
+dest_dir=$images_dir"/reduced_"$2
+
+echo " --------------- "
+echo "  reduce_images  "
+echo " --------------- "
+
+echo "-- Processing image directory: "$images_dir
+echo "-- Destination directory: "$dest_dir
+echo "-- Reduce size will be: "$perc"%"
+
+mkdir -p $dest_dir
+
+shopt -s nocaseglob
+
+total=$(ls -1 $images_dir/*.jpg | wc -l)
+counter=0
+
+for i in $(ls $images_dir/*.jpg); do
+    echo -ne "converting ${i##*/}  ("$counter"/"$total") \r"
+    convert -resize "$perc%" $i $dest_dir/${i##*/}
+    counter=$(($counter+1))
 done
 
+echo "\n"
--- restart_wifi.sh
+++ restart_wifi.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-sudo systemctl restart NetworkManager
+sudo systemctl restart NetworkManager.service
