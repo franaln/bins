ba0489d16d1f2bd06d177f5c776512f5f95eb74f
franaln <franaln@gmai.com>
2014-03-05 12:27:17 +0100

    update

--- conf_touchpad.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-synclient "TapButton2=3" "TapButton3=2" "HorizEdgeScroll=on" "HorizTwoFingerScroll=1"
-#synclient TapButton2=0 TapButton3=0 ClickFinger2=0 ClickFinger3=0 HorizTwoFingerScroll=0 VertTwoFingerScroll=0
