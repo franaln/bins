97f28989da2808e4eb066c8ad870aa81a815dc02
franaln <franaln@gmai.com>
2014-05-02 01:47:18 +0200

    update

--- touchpad.sh
+++ touchpad.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/env bash
 
 # standalone
-# synclient TapButton1=1
-# synclient TapButton2=3
-# synclient TapButton3=2
-# synclient ClickFinger2=0
-# synclient ClickFinger3=0
-# synclient HorizEdgeScroll=1
-# synclient HorizTwoFingerScroll=1
-# synclient VertTwoFingerScroll=1
-
-# if touchegg
-synclient TapButton2=0
-synclient TapButton3=0
+synclient TapButton1=1
+synclient TapButton2=3
+synclient TapButton3=2
 synclient ClickFinger2=0
 synclient ClickFinger3=0
-synclient HorizTwoFingerScroll=0
-synclient VertTwoFingerScroll=0
+synclient HorizEdgeScroll=1
+synclient HorizTwoFingerScroll=1
+synclient VertTwoFingerScroll=1
+
+# if touchegg
+# synclient TapButton2=0
+# synclient TapButton3=0
+# synclient ClickFinger2=0
+# synclient ClickFinger3=0
+# synclient HorizTwoFingerScroll=0
+# synclient VertTwoFingerScroll=0
