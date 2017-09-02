e3261acf77c571d9eb4c511f6b20c5dab464e73a
Francisco Alonso <franaln@gmail.com>
2017-09-02 20:42:12 +0200

    upate

--- touchpad.sh
+++ touchpad.sh
@@ -6,17 +6,39 @@ synclient TapButton2=3
 synclient TapButton3=2
 synclient ClickFinger2=0
 synclient ClickFinger3=0
-synclient HorizEdgeScroll=1
+
 synclient HorizTwoFingerScroll=1
 synclient VertTwoFingerScroll=1
 
-#synclient FingerLow=30
-#synclient FingerHigh=32
+synclient FingerLow=30
+synclient FingerHigh=35
 
-# if touchegg
-# synclient TapButton2=0
-# synclient TapButton3=0
-# synclient ClickFinger2=0
-# synclient ClickFinger3=0
-# synclient HorizTwoFingerScroll=0
-# synclient VertTwoFingerScroll=0
+synclient MinSpeed=1
+synclient MaxSpeed=4
+synclient AccelFactor=0.06
+
+synclient VertScrollDelta=50
+synclient HorizScrollDelta=50
+
+# synclient MaxTapTime=180
+# synclient MaxTapMove=170
+# synclient MaxDoubleTapTime=180
+# synclient EmulateTwoFingerMinZ=40
+# synclient EmulateTwoFingerMinW=7
+# synclient SingleTapTimeout        = 180
+# synclient ClickTime               = 100
+# synclient EmulateMidButtonTime    = 0
+# synclient VertEdgeScroll          = 0
+# synclient HorizEdgeScroll         = 1
+# synclient CornerCoasting          = 0
+# synclient LockedDrags             = 0
+# synclient LockedDragTimeout       = 5000
+# synclient CoastingSpeed           = 20
+# synclient CoastingFriction        = 50
+# synclient PressureMotionMinZ      = 30
+# synclient PressureMotionMaxZ      = 160
+# synclient PressureMotionMinFactor = 1
+# synclient PressureMotionMaxFactor = 1
+# synclient HorizHysteresis         = 19
+# synclient VertHysteresis          = 19
+# synclient ClickPad                = 1
